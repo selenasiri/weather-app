2d0d439e2d7b7fe4f0f5758bd6c478cb48fc68e4
Selena Siri <selenasiri@gmail.com>
2020-11-02 15:15:20 -0500

    resized background image

--- src/App.js
+++ src/App.js
@@ -56,27 +56,22 @@ const App = () => {
             onKeyPress={search}
             />
         </div>
-
         {typeof weather.main === "undefined"  && ('')}
-
         {(typeof weather.main !== "undefined") && (
           <div>
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
-
             <div className="weather-box">
               <div className="temp">
                 {Math.round(weather.main.temp)}°c
               </div>
-
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
           )
         }
-
       </main>
     </div>
   )
