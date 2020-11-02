386d5836dc07a7630d9bf22a1030d64f28d58d27
Selena Siri <selenasiri@gmail.com>
2020-11-02 12:25:33 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+//api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
 import React from 'react'
+const api = {
+  key: "a25bf647af5a1e26d79456d892a32166",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+      </main>
     </div>
   )
 }
